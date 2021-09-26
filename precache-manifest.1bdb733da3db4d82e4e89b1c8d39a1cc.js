3b2f537ee7d12156b63b5915fc1a7719867dbce7
iForced <iforce@2ch.hk>
2021-09-26 17:17:15 +0300

    Updates

---
+++ precache-manifest.1bdb733da3db4d82e4e89b1c8d39a1cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c92e5d912833fa19a4586ecde801715",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "916206f50c58862ef876",
+    "url": "/todoList/static/css/main.7258550c.chunk.css"
+  },
+  {
+    "revision": "8cb106eb174865c47f34",
+    "url": "/todoList/static/js/2.352649fa.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todoList/static/js/2.352649fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916206f50c58862ef876",
+    "url": "/todoList/static/js/main.547dd5e4.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
